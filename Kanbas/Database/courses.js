0099821946c7a65ea27597fb78bda71c53b7b841
aishwaryakeskar <aishwaryakeskar98@gmail.com>
2024-04-04 20:43:01 -0400

    Update Kanbas

---
+++ Kanbas/Database/courses.js
@@ -0,0 +1,13 @@
+export default [
+  { "_id": "RS101", "name": "Rocket Propulsion", "number": "RS4550", "startDate": "2023-01-10",
+    "endDate": "2023-05-15", "image": "../../images/aerodynamics.jpeg"  },
+  { "_id": "RS102", "name": "Aerodynamics", "number": "RS4560", "startDate": "2023-01-10",
+    "endDate": "2023-05-15", "image": "../../images/rocket-propulsion.jpeg"  },
+  { "_id": "RS103", "name": "Spacecraft Design", "number": "RS4570", "startDate": "2023-01-10",
+    "endDate": "2023-05-15", "image":"../images/spacecraft-design.jpeg"  },
+  { "_id": "RS104", "name": "Web Development", "number": "RS4551", "startDate": "2023-01-10",
+    "endDate": "2023-05-15", "image": "../../images/webdev.jpg"  },
+  { "_id": "RS105", "name": "Project Management", "number": "RS4561", "startDate": "2023-01-10",
+    "endDate": "2023-05-15", "image": "../../images/project-management.jpg"  },
+  { "_id": "RS106", "name": "ReactJS", "number": "RS4571", "startDate": "2023-01-10",
+    "endDate": "2023-05-15", "image":"../images/reactjs.jpeg"  } ]
