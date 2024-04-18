adadb60e1d26350fbf02f2efcd8790bcbe0ba898
aishwaryakeskar <aishwaryakeskar98@gmail.com>
2024-04-18 14:41:55 -0400

    Add mongodb connection

--- Kanbas/modules/routes.js
+++ Kanbas/modules/routes.js
@@ -1,38 +1,38 @@
+import courses from "../Database/courses.js";
 import db from "../Database/index.js";
-function ModuleRoutes(app) {
-    app.delete("/api/modules/:mid", (req, res) => {
-        const { mid } = req.params;
-        db.modules = db.modules.filter((m) => m._id !== mid);
-        res.sendStatus(200);
-      });
+import * as dao from "./dao.js";
 
-      app.put("/api/modules/:mid", (req, res) => {
-        const { mid } = req.params;
-        const moduleIndex = db.modules.findIndex(
-          (m) => m._id === mid);
-        db.modules[moduleIndex] = {
-          ...db.modules[moduleIndex],
-          ...req.body
-        };
-        res.sendStatus(204);
-      });
-    
-    app.post("/api/courses/:cid/modules", (req, res) => {
+export default function ModuleRoutes(app) {
+    app.get("/api/courses/:cid/modules", async (req, res) =>  {
+        console.log("in");
+        const { cid } = req.params;
+        const modules = await dao.getCourseModules(cid);
+        console.log(modules)
+        res.send(modules);
+    });
+
+    app.post("/api/courses/:cid/modules", async (req, res) => {
         const { cid } = req.params;
         const newModule = {
-          ...req.body,
-          course: cid,
-          _id: new Date().getTime().toString(),
+            ...req.body,
+            course: cid,
+            _id: new Date().getTime().toString(),
         };
-        db.modules.push(newModule);
-        res.send(newModule);
-      });
-    
-  app.get("/api/courses/:cid/modules", (req, res) => {
-    const { cid } = req.params;
-    const modules = db.modules
-      .filter((m) => m.course === cid);
-    res.send(modules);
-  });
+        const module = await dao.createModule(newModule);
+        res.send(module);
+    });
+
+    app.delete("/api/modules/:mid", async (req, res) => {
+        const { mid } = req.params;
+        await dao.deleteModule(mid);    
+        res.sendStatus(200);
+    });
+
+    app.put("/api/modules/:mid", async (req, res) => {
+        const { mid } = req.params;
+        console.log(mid, req.body)
+        const newmod = await dao.updateModule(mid, req.body)
+        console.log(newmod);
+        res.sendStatus(204);
+    });
 }
-export default ModuleRoutes;
