74f5fca28750354959e90f154935b5d546a3a77f
aishwaryakeskar <aishwaryakeskar98@gmail.com>
2024-04-18 16:20:44 -0400

    bug fix

--- App.js
+++ App.js
@@ -1,24 +1,25 @@
-import express from 'express'
-import mongoose from "mongoose";
-import UserRoutes from "./Users/routes.js";
-import Hello from "./Hello.js"
-import Lab5 from "./Lab5.js";
+import express from 'express';
+import Hello from './Hello.js';
+import Lab5 from './Lab5.js';
 import cors from "cors";
-import CourseRoutes from "./Kanbas/courses/routes.js";
+import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
+import UserRoutes from "./Kanbas/users/routes.js";
+import mongoose from "mongoose";
 import session from "express-session";
 import "dotenv/config";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
-mongoose.connect(CONNECTION_STRING);
-const app = express()
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL
-  })
-);
+const app = express();
+const corsOptions = {
+  origin: process.env.FRONTEND_URL, // or use true to reflect the request origin, as long as it's not wildcard *
+  credentials: true // This is important for credentials
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
 const sessionOptions = {
   secret: process.env.SESSION_SECRET,
   resave: false,
@@ -34,11 +35,15 @@ if (process.env.NODE_ENV !== "development") {
 }
 app.use(session(sessionOptions));
 
-app.use(express.json());
-ModuleRoutes(app);
-CourseRoutes(app);
-AssignmentRoutes(app);
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING;
+mongoose.connect(CONNECTION_STRING);
+
+Hello(app);
 Lab5(app);
-Hello(app)
+CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
 UserRoutes(app);
+
 app.listen(process.env.PORT || 4000);
